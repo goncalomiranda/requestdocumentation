c073238d93382ed612fbdd422259c081fcd71eef
Goncalo Miranda <miranda.gs@gmail.com>
2025-02-16 00:07:25 -0500

    update app tsx
    update app tsx

--- upload-documents/src/App.tsx
+++ upload-documents/src/App.tsx
@@ -16,36 +16,6 @@ function App() {
       </Routes>
     </Router>
   );
-
-//   return (
-//     <>
-
-// <main>
-//       <div
-//         className="bg-dark text-secondary px-4 py-5 text-center"
-//         id="mainDivDocumentation"
-//       >
-//         <div className="py-5">
-//           <h1 className="display-5 fw-bold text-white">Page Title</h1>
-//           <div className="col-lg-6 mx-auto">
-//             <p className="fs-5 mb-4">
-//               Upload required documents securely and efficiently. Select files,
-//               provide optional details, and submit them for processing—all in a
-//               user-friendly interface.
-//             </p>
-//             <div
-//               className="d-grid gap-2 d-sm-flex justify-content-sm-center"
-//               id="listOfDocuments"
-//             >
-//               <UploadPage />
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </main>
-
-//     </>
-//   )
  }
 
 export default App
