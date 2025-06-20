4bc1ae160e1daa1b41df8c6b5673993ad1a51fcc
Goncalo Miranda <miranda.gs@gmail.com>
2025-06-19 23:57:11 -0400

    clean up unused files
    clean up unused files

--- upload-documents/src/App.tsx
+++ upload-documents/src/App.tsx
@@ -1,16 +1,13 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-//import './App.css'
 import './uploadpage.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UploadPage from './components/uploadpageComponent';
 
 function App() {
-
+  
   return (
     <Router>
+
       <Routes>
         <Route path="/upload" element={<UploadPage />} />
       </Routes>
