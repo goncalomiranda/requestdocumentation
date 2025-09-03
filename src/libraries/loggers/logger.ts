3b3772e324e89a43aca0a7ede8372c68f91c69bf
Goncalo Miranda <miranda.gs@gmail.com>
2025-09-03 05:55:36 -0400

    add loggers
    add loggers

--- src/libraries/loggers/logger.ts
+++ src/libraries/loggers/logger.ts
@@ -1,25 +1,51 @@
 import winston from 'winston';
+import { Logtail } from '@logtail/node';
+import { LogtailTransport } from '@logtail/winston';
+import dotenv from "dotenv";
+
+// Get environment for logging
+const environment = process.env.NODE_ENV || 'development';
 
 // Define log format for file logs (plain text)
-const logFormat = winston.format.printf(({ timestamp, level, message }) => {
-  return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+const logFormat = winston.format.printf(({ timestamp, level, message, service }) => {
+  return `${timestamp} [${environment.toUpperCase()}] [${service}] [${level.toUpperCase()}]: ${message}`;
+});
+
+// Check if required environment variables are set
+const logToken = process.env.LOG_TOKEN;
+const logHost = process.env.LOG_HOST;
+
+if (!logToken) {
+  throw new Error('LOG_TOKEN environment variable is required');
+}
+
+if (!logHost) {
+  throw new Error('LOG_HOST environment variable is required');
+}
+
+const logtail = new Logtail(logToken, {
+  endpoint: `https://${logHost}`,
 });
 
 // Create Winston Logger
 const logger = winston.createLogger({
-  level: 'info',
+  level: process.env.LOG_LEVEL || 'info',
+  defaultMeta: {
+    service: 'requestdocumentation-service',
+    environment: environment,
+  },
   format: winston.format.combine(
     winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), // Standard timestamp format
-    winston.format.printf(({ timestamp, level, message }) => `${timestamp} [${level.toUpperCase()}]: ${message}`)
+    winston.format.printf(({ timestamp, level, message, service }) => `${timestamp} [${environment.toUpperCase()}] [${service}] [${level.toUpperCase()}]: ${message}`)
   ),
   transports: [
     // Console logs (with colors)
     new winston.transports.Console({
       format: winston.format.combine(
-        winston.format.printf(({ timestamp, level, message }) => `${timestamp} [${level.toUpperCase()}]: ${message}`)
+        winston.format.printf(({ timestamp, level, message, service }) => `${timestamp} [${environment.toUpperCase()}] [${service}] [${level.toUpperCase()}]: ${message}`)
       ),
     }),
-    
+
     // File logs (plain text, no colors)
     new winston.transports.File({
       filename: 'logs/app.log',
@@ -28,6 +54,7 @@ const logger = winston.createLogger({
         logFormat // Plain text format for file logs
       ),
     }),
+    new LogtailTransport(logtail),
   ],
 });
 
