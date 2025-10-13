27cc1e1269a817c92980f54b774e4f12a6006c76
Goncalo Miranda <miranda.gs@gmail.com>
2025-10-12 23:37:27 -0400

    add create app form - base 1
    add create app form - base 1

---
+++ src/apps/request-documentation/domain/models/RequestDocumentationUpdate.ts
@@ -0,0 +1,8 @@
+// Domain model for request documentation update
+interface RequestDocumentationUpdateModel {
+  request_id: string;
+  status: string;
+}
+
+
+export { RequestDocumentationUpdateModel };
